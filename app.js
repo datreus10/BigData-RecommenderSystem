f7d518b84b4aa03221d2f0e84b561c8e8723b85a
Tô Quang Huy <18520856@gm.uit.edu.vn>
2021-12-28 15:08:32 +0700

    Connect database

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var connectDatabase = require('./Config/ConnectDatabase')
+connectDatabase()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pythonopen = require('./routes/pythonopen');
