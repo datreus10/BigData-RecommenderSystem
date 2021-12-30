2848e1f1ed38dd5d0a13f6bec8fdd4c2653857d0
Đạt Nguyễn Thành <18520580@gm.uit.edu.vn>
2021-12-30 11:20:28 +0700

    update home page and detail page

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,13 +1,45 @@
-const categorytemp = ['Action/Adventure','Kids & Family',	'Medical','Military/War','Music,Musical',	'Mystery/Crime',	'Nature','Paranormal','Politics','Racing','Romance','Science','ScienceFiction','Science/Nature','Spanish','Travel','Western']
+const categorytemp = ['Action/Adventure', 'Kids & Family', 'Medical', 'Military/War', 'Music,Musical', 'Mystery/Crime', 'Nature', 'Paranormal', 'Politics', 'Racing', 'Romance', 'Science', 'ScienceFiction', 'Science/Nature', 'Spanish', 'Travel', 'Western']
 
-const listmovie = [{name: 'Fast and furious 7', star: '10'},{name:'John Wick 3', star: '10'},{name:'John Cenna',star: '10'},{name: 'Fast and furious 7', star: '10'},{name:'John Wick 3', star: '10'},{name:'John Cenna',star: '10'},{name: 'Fast and furious 7', star: '10'},{name:'John Wick 3', star: '10'},{name:'John Cenna',star: '10'}]
+const listmovie = [{
+  name: 'Fast and furious 7',
+  star: '10'
+}, {
+  name: 'John Wick 3',
+  star: '10'
+}, {
+  name: 'John Cenna',
+  star: '10'
+}, {
+  name: 'Fast and furious 7',
+  star: '10'
+}, {
+  name: 'John Wick 3',
+  star: '10'
+}, {
+  name: 'John Cenna',
+  star: '10'
+}, {
+  name: 'Fast and furious 7',
+  star: '10'
+}, {
+  name: 'John Wick 3',
+  star: '10'
+}, {
+  name: 'John Cenna',
+  star: '10'
+}]
 
 const getmoviespage = (req, res, next) => {
-  res.render("movies",{category: categorytemp, movies: listmovie});
+  res.render("movies", {
+    category: categorytemp,
+    movies: listmovie
+  });
 };
 
 const detailMovie = (req, res, next) => {
-  res.render("detail");
+  res.render("detail", {
+    user: req.user
+  });
 };
 
 const search = (req, res, next) => {
