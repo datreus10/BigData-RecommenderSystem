44a10f0378168ef6caed3d5afb33063493b5a3a7
Đạt Nguyễn Thành <18520580@gm.uit.edu.vn>
2021-12-30 12:41:19 +0700

    add review route

--- controllers/movies.js
+++ controllers/movies.js
@@ -32,14 +32,50 @@ const listmovie = [{
 const getmoviespage = (req, res, next) => {
   res.render("movies", {
     category: categorytemp,
-    movies: listmovie
+    movies: listmovie,
+    user: req.user
   });
 };
 
 const detailMovie = (req, res, next) => {
   res.render("detail", {
-    user: req.user
+    user: req.user,
+    recMovies: [1, 2, 3, 4, 5]
   });
+};
+
+
+
+const parseReview = (data) => {
+  const rating = parseFloat(data.rating.replace(' and a half', '.5'));
+  const isValid = Object.values(data).every(value => {
+    if (value) {
+      return true;
+    }
+    return false;
+  });
+  if (!isValid || !rating)
+    throw 'Data review not valid';
+  return {
+    title: data.title,
+    review: data.review,
+    rating: rating
+  }
+
+}
+
+const reviewMovie = (req, res, next) => {
+  try {
+    const review = parseReview(req.body);
+    console.log(review);
+    res.render("detail", {
+      user: req.user
+    });
+  } catch (error) {
+    console.log(error);
+    next(createError(404));
+  }
+
 };
 
 const search = (req, res, next) => {
@@ -49,9 +85,11 @@ const search = (req, res, next) => {
 const filter = (req, res, next) => {
   res.send(req.body);
 };
+
 module.exports = {
   getmoviespage,
   search,
   filter,
   detailMovie,
+  reviewMovie
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var {getmoviespage,search,filter,detailMovie} = require('../controllers/movies');
+var {
+    getmoviespage,
+    search,
+    filter,
+    detailMovie,
+    reviewMovie
+} = require('../controllers/movies');
 /* GET home page. */
 router.get('/', getmoviespage);
-router.get('/detail',detailMovie);
+router.get('/detail', detailMovie);
+router.post('/review', reviewMovie);
 router.post('/search', search);
 router.post('/filter', filter);
 
