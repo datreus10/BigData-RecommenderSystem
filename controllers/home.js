eebe84e0485382222132bca6611eeef45a7748bf
Đạt Nguyễn Thành <18520580@gm.uit.edu.vn>
2022-01-01 10:38:47 +0700

    update loadrec

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,7 @@
 const movieAPI = require("../helper/movieAPI");
-const { spawn } = require("child_process");
+const {
+  spawn
+} = require("child_process");
 
 const testMovies = [
   "Toy Story (1995)",
@@ -10,29 +12,40 @@ const testMovies = [
 ];
 
 const getHomePage = (req, res, next) => {
-  const userId = req.query.userId;
+  const userId = req.query.userId || 1;
 
   let result;
 
-  const python = spawn("python3", ["./recommendation/loadRec.py", userId]);
+  const python = spawn("python", ["./recommendation/loadRec.py", userId]);
   // collect data from script
   python.stdout.on("data", function (data) {
-    result = data.toString();
+    result += data.toString();
   });
   // in close event we are sure that stream from child process is closed
   python.on("close", async (code) => {
-    result = JSON.parse(result);
 
-    res.render("index", {
-      movies: await movieAPI.getMoviesById(result["tmdbId"]),
-      recommend: result,
-      user: req.user,
-    });
-  });
+    try {
+      const idx1 = result.indexOf("{")
+      const idx2 = result.indexOf("}")
+      if (idx1 > -1 && idx2 > -1)
+        result = result.substring(idx1, idx2 + 1)
+      result = JSON.parse(result);
+    } catch (error) {
+      console.log(error)
+    } finally {
+      res.send(result);
+    }
+
     // res.render("index", {
-    //   movies: testMovies,
+    //   movies: await movieAPI.getMoviesById(result["tmdbId"]),
+    //   recommend: result,
     //   user: req.user,
     // });
+  });
+  // res.render("index", {
+  //   movies: testMovies,
+  //   user: req.user,
+  // });
 };
 
 module.exports = {
